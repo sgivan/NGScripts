855935433a156e998509a7d143c35580cdf3177c
Scott A. Givan <sgivan@yahoo.com>
2014-09-19 11:28:21 -0500

    Made check_coverage.sh more flexible. It now looks for *.delta files in subdirectories and tailors output accordingly.

--- bin/check_coverage.sh
+++ bin/check_coverage.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-# copy this script from ~sgivan/projects/NGSscripts/bin
-# developed for use with nucmer output
 
 file=$1
 
-echo "|Contig|ANT505|AUTQ|AUTS|BADV|NJ631|CAPN|"
+dirs=`ls -C */*.delta | sed -r 's/\/\w+.delta//g' | sed 's/  / /g'`
+header=`echo $dirs | sed 's/ /|/g'`
+
+#echo "dirs: $dirs"
+#echo "header: |$header|"
+#exit
+
+#echo "|Contig|JJNZ|AUTP|JEMJ|AUTQ|"
+echo "|Contig|$header|"
 
 for node in `grep '>' $file | sed 's/>//' | sed -r 's/_length.+//' | sort -g`
 do
     printf '|%s' $node
-    for dir in ANT505 AUTQ AUTS BADV NJ631 CAPN 
+    #for dir in JJNZ AUTP JEMJ AUTQ
+    for dir in $dirs
         do
             cov=`show-coords -L 1000 -c -l -q -T ${dir}/${dir}.delta | grep ${node}_ | datamash sum 11`
             printf '|%2.2f' $cov
